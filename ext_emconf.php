b374a829c5e041f6dfa459286e52f511e674df11
René Nitzsche <rene@system25.de>
2022-06-06 14:03:44 +0200

    Raise version

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@
 # "version" and "dependencies" must not be touched!
 ########################################################################
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
 	'title' => 'REST for T3sports',
 	'description' => 'Provides a REST interface for T3sports.',
 	'category' => 'fe',
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_email' => 'rene@system25.de',
 	'author_company' => 'System 25',
 	'shy' => '',
-	'version' => '0.1.0',
+	'version' => '0.2.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -30,18 +30,13 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'constraints' => array(
-		'depends' => array(
-			'cms' => '',
-			'rn_base' => '0.11.0-0.0.0',
-			't3rest' => '0.1.0-0.0.0',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
+	'constraints' => [
+		'depends' => [
+			'rn_base' => '1.11.0-0.0.0',
+			't3rest' => '1.1.0-0.0.0',
+		],
+		'conflicts' => [],
+		'suggests' => [],
+	],
 	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"690e";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"fc02";s:19:"doc/wizard_form.dat";s:4:"d0ae";s:20:"doc/wizard_form.html";s:4:"1048";s:23:"static/ts/constants.txt";s:4:"96ef";s:19:"static/ts/setup.txt";s:4:"f531";}',
-);
-
-?>
+];
